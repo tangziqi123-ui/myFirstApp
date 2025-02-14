876bd7b782e3a8bc9b9d2838b06c112c71d4788f
tangziqi123-ui <tangziqi0222@sina.com>
2025-02-14 16:14:41 +0800

    Add files via upload

---
+++ ckd.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2025/2/12 21:45
+# @Author  : Ken
+# @Software: PyCharm
+import joblib
+import streamlit as st
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+
+
+def load_and_process_data(sheet_name, input_x):
+    train = np.load(f'train_{sheet_name}.npy')
+    test = np.load(f'test_{sheet_name}.npy')
+    data = np.concatenate((train, test), axis=0)
+    data = pd.DataFrame(data)
+    x = np.array(input_x)
+    print(x)
+    X = data.iloc[:, 1:].values
+
+    x_binary = x[:, -3:]
+    x_binary_encoded = pd.get_dummies(pd.DataFrame(x_binary)).values
+
+    X_continuous = X[:, :-3]
+    x_continuous = x[:, :-3]
+    scaler = StandardScaler()
+    scaler.fit(X_continuous)
+    x_continuous_scaled = scaler.transform(x_continuous)
+    x_processed = np.hstack([x_continuous_scaled, x_binary_encoded])
+
+    return x_processed
+
+st.set_page_config(
+    layout='wide'
+)
+
+# 标题,居中
+st.markdown(
+    "<h1 style='text-align: center;'> <span style='color: green;'>Prediction System for</span><span style='color: red;'> Chronic Kidney Disease（CKD）Stage</span></h1>",
+    unsafe_allow_html=True)
+
+st.markdown(
+    "<h1 style='text-align: center; color: green;'>In Obstructive Sleep Apnea（OSA） Patients</h1>",
+    unsafe_allow_html=True)
+
+st.markdown("<h1 style='text-align: center; color: green;'></h1>", unsafe_allow_html=True)
+left, right = st.columns(2)
+with left:
+    A = st.selectbox('OSA Severity', ['Mild', 'Moderate', 'Severe'])
+    if A == 'Mild':
+        A = 1
+    elif A == 'Moderate':
+        A = 2
+    else:
+        A = 3
+    Q = st.number_input('Apnea-Hypopnea Index（events/hour）', max_value=5, min_value=0)
+    O = st.number_input('Slowest Pulse Rate(bpm)', max_value=100, min_value=60)
+    E = st.number_input('Age（years）', max_value=200, min_value=0)
+    U = st.number_input('Body Mass Index（kg/m²）', max_value=9999.00, min_value=0.00)
+    P = st.selectbox('Body Type', ['Normal or Thinness', 'Overweightness', 'Obesity'])
+    if P == 'Normal or Thinness':
+        P = 0
+    elif P == 'Overweightness':
+        P = 1
+    elif P == 'Obesity':
+        P = 2
+    B = st.selectbox('Hypertension',  ['No', 'Yes'])
+    if B == 'No':
+        B = 0
+    else:
+        B = 1
+    D = st.selectbox('Diabetes Mellitus',  ['No', 'Yes'])
+    if D == 'No':
+        D = 0
+    else:
+        D = 1
+    C = st.selectbox('Coronary Heart Disease',  ['No', 'Yes'])
+    if C == 'No':
+        C = 0
+    else:
+        C = 1
+    F = st.number_input('Hemoglobin(g/L)', max_value=150, min_value=110)
+    G = st.number_input('Lymphocytes(×10⁹/L)', max_value=0.6, min_value=0.1)
+    H = st.number_input('Platelet Count(×10⁹/L)', max_value=350, min_value=125)
+    R = st.number_input('Serum Albumin(g/L)', max_value=9999.00, min_value=0.00)
+with right:
+    S = st.number_input('Alkaline Phosphatase(U/L)', max_value=9999.00, min_value=0.00)
+    T = st.number_input('Parathyroid Hormone(pmol/L)', max_value=55, min_value=40)
+    V = st.number_input('Cystatin C(mg/L)', max_value=1.03, min_value=0.00)
+    W = st.number_input('Blood Creatinine(umol/L)', max_value=81, min_value=41)
+    X = st.number_input('Blood Urea Nitrogen(mmol/L)', max_value=8.8, min_value=3.1)
+    Y = st.number_input('β2-Microglobulin(mg/L)', max_value=3.0, min_value=1.3)
+    M = st.number_input('Total Cholesterol（mmol/L）', max_value=5.18, min_value=2.80)
+    N = st.number_input('Low-Density Lipoprotein Cholesterol（mmol/L）', max_value=3.37, min_value=2.00)
+    I = st.number_input('Blood Potassium（mmol/L）', max_value=5.3, min_value=3.5)
+    J = st.number_input('Blood Phosphorus（mmol/L）', max_value=1.51, min_value=0.85)
+    K = st.number_input('Calcium-Phosphorus Product(mmol²/L²)', max_value=30.00, min_value=0.00)
+    L = st.number_input('Blood Magnesium（mmol/L）', max_value=1.02, min_value=0.75)
+
+st.markdown("<p style='text-align: center; color: green;'></p>", unsafe_allow_html=True)
+
+if st.button('CKD Stage Predict Outcomes'):
+    ckd_model = joblib.load('CKD_best_xgb_model.pkl')
+    input_x = [[A, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, B, C, D]]
+    print(input_x)
+    x = load_and_process_data('CKD', input_x)
+    y_pred = ckd_model.predict(x)
+    # y_prob = ckd_model.predict_proba(x)[:, 1]
+    y_prob = ckd_model.predict_proba(x)
+    print(y_pred)
+    print(y_prob)
+
+    res_value = 'Stage: ' + str(y_pred[0])
+    res_proba = 'Probability: ' + str(y_prob[0])
+
+    df = pd.DataFrame(y_prob[0]).reset_index()
+    df.columns = ['Stage', 'Probability']
+    map_result = {0: 'Stage 1', 1: 'Stage 2', 2: 'Stage 3', 3: 'Stage 4', 4: 'Stage 5'}
+    df['Stage'] = df['Stage'].apply(lambda xx: map_result[xx])
+    df['Probability'] = df['Probability'].apply(lambda xx: str(round(xx * 100, 2)) + '%')
+
+    df.index += 1
+    # 选择要高亮的列
+    target_column = "Probability"  # 这里指定列名
+    # 找到最大值所在的行索引
+    max_row_idx = df[target_column].apply(lambda xx: float(xx.replace('%', ''))).idxmax()
+
+    # 自定义样式函数
+    def highlight_max_row(row):
+        return ['background-color: #FF9999' if row.name == max_row_idx else '' for _ in row]
+
+
+    # 应用样式
+    styled_df = df.style.apply(highlight_max_row, axis=1)
+
+    # 在 Streamlit 中显示
+    st.table(styled_df)
+
---
+++ osa.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2025/2/12 21:45
+# @Author  : Ken
+# @Software: PyCharm
+import joblib
+import streamlit as st
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+
+def load_and_process_data(sheet_name, input_x):
+    train = np.load(f'train_{sheet_name}.npy')
+    test = np.load(f'test_{sheet_name}.npy')
+    data = np.concatenate((train, test), axis=0)
+    data = pd.DataFrame(data)
+    x = np.array(input_x)
+    print(x[0])
+
+    X = data.iloc[:, 1:].values
+
+    x_binary = x[:, -3:]
+    x_binary_encoded = pd.get_dummies(pd.DataFrame(x_binary)).values
+
+    X_continuous = X[:, :-3]
+    x_continuous = x[:, :-3]
+    scaler = StandardScaler()
+    scaler.fit(X_continuous)
+    x_continuous_scaled = scaler.transform(x_continuous)
+    x_processed = np.hstack([x_continuous_scaled, x_binary_encoded])
+
+    return x_processed
+
+
+st.set_page_config(
+    layout='wide'
+)
+
+# 标题,居中
+st.markdown(
+    "<h1 style='text-align: center;'> <span style='color: green;'>Prediction System for </span><span style='color: red;'>Obstructive Sleep Apnea（OSA）Severity</span></h1",
+    unsafe_allow_html=True)
+st.markdown(
+    "<h1 style='text-align: center; color: green;'>In Chronic Kidney Disease（CKD） Patients</h1",
+    unsafe_allow_html=True)
+
+st.markdown("<h1 style='text-align: center; color: green;'></h1>", unsafe_allow_html=True)
+
+left, right = st.columns(2)
+with left:
+    B = st.selectbox('CKD Stage', ['Stage 1', 'Stage  2', 'Stage 3', 'Stage 4', 'Stage 5'])
+    B = int(B.replace('Stage ', ''))
+    C = st.number_input('Body Mass Index（kg/m²）', max_value=9999.00, min_value=0.00)
+    A = st.selectbox('Body Type', ['Normal or Thinness', 'Overweightness', 'Obesity'])
+    if A == 'Normal or Thinness':
+        A = 0
+    elif A == 'Overweightness':
+        A = 1
+    elif A == 'Obesity':
+        A = 2
+    K = st.selectbox('Hypertension', ['No', 'Yes'])
+    if K == 'No':
+        K = 0
+    else:
+        K = 1
+    L = st.selectbox('Diabetes Mellitus', ['No', 'Yes'])
+    if L == 'No':
+        L = 0
+    else:
+        L = 1
+    J = st.selectbox('Alcohol Consumption History', ['No', 'Yes'])
+    if J == 'No':
+        J = 0
+    else:
+        J = 1
+
+with right:
+    H = st.number_input('White Blood Cell(×10⁹/L)', max_value=9.5, min_value=3.5)
+    I = st.number_input('Neutrophils(×10⁹/L)', max_value=6.3, min_value=1.8)
+    G = st.number_input('Blood Creatinine(umol/L)', max_value=81, min_value=41)
+    D = st.number_input('Cystatin C(mg/L)', max_value=1.03, min_value=0.00, )
+    E = st.number_input('Glomerular Filtration Rate（mL/min/1.73m²）', max_value=9999.00, min_value=90.00)
+    F = st.number_input('Total Carbon Dioxide in Blood（mmol/L）', max_value=30, min_value=20)
+
+st.markdown("<p style='text-align: center; color: green;'></p>", unsafe_allow_html=True)
+
+if st.button('OSA Severity Predict Outcomes'):
+    osa_model = joblib.load('OSA_best_xgb_model.pkl')
+    input_x = [[A, B, C, D, E, F, G, H, I, J, K, L]]
+    print(input_x)
+    x = load_and_process_data('OSA', input_x)
+    print(x)
+    y_pred = osa_model.predict(x)
+    y_prob = osa_model.predict_proba(x)
+    print(y_pred)
+    print(y_prob)
+    res_value = 'Severity: ' + str(y_pred[0])
+    res_proba = 'Probability: ' + str(y_prob[0])
+
+    df = pd.DataFrame(y_prob[0]).reset_index()
+    df.columns = ['Severity', 'Probability']
+    map_result = {0: 'Mild', 1: 'Moderate', 2: 'Severe'}
+    df['Severity'] = df['Severity'].apply(lambda xx: map_result[xx])
+    df['Probability'] = df['Probability'].apply(lambda xx: str(round(xx * 100, 2)) + '%')
+    df.index += 1
+
+    # 选择要高亮的列
+    target_column = "Probability"  # 这里指定列名
+    # 找到最大值所在的行索引
+    max_row_idx = df[target_column].apply(lambda xx: float(xx.replace('%',''))).idxmax()
+    # 自定义样式函数
+    def highlight_max_row(row):
+        return ['background-color: #FF9999' if row.name == max_row_idx else '' for _ in row]
+    # 应用样式
+    styled_df = df.style.apply(highlight_max_row, axis=1)
+
+    # 在 Streamlit 中显示
+    st.table(styled_df)
